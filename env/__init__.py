362aa773d88d05d4e9cec1bcee143468e857ca7a
Jacob1we <50640416+Jacob1we@users.noreply.github.com>
2026-02-03 10:27:10 +0100

    Mujoco als Optional ind der planning config

--- env/__init__.py
+++ env/__init__.py
@@ -15,19 +15,22 @@ register(
     max_episode_steps=300,
     reward_threshold=1.0,
 )
-register(
-    id='point_maze',
-    entry_point='env.pointmaze:PointMazeWrapper',
-    max_episode_steps=300,
-    kwargs={
-        'maze_spec':U_MAZE,
-        'reward_type':'sparse',
-        'reset_target': False,
-        'ref_min_score': 23.85,
-        'ref_max_score': 161.86,
-        'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-umaze-sparse-v1.hdf5'
-    }
-)
+
+# point_maze nur registrieren wenn MuJoCo verfügbar
+if _HAS_MUJOCO:
+    register(
+        id='point_maze',
+        entry_point='env.pointmaze:PointMazeWrapper',
+        max_episode_steps=300,
+        kwargs={
+            'maze_spec':U_MAZE,
+            'reward_type':'sparse',
+            'reset_target': False,
+            'ref_min_score': 23.85,
+            'ref_max_score': 161.86,
+            'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-umaze-sparse-v1.hdf5'
+        }
+    )
 register(
     id="wall",
     entry_point="env.wall.wall_env_wrapper:WallEnvWrapper",
